db773a24c516bab91d1bfe8decb159c998266194
Komei Kamiya <komei.kamiya@gmail.com>
2022-09-12 23:34:19 +0900

    refactor: create module related to company

--- employee_list/src/main.rs
+++ employee_list/src/main.rs
@@ -1,131 +1,134 @@
 use std::{io::{self, Write}, collections::HashMap};
 
-#[derive(Debug)]
-enum Operation {
-    GetAll, 
-    GetDepartment(String), // department :String:
-    Add(String, String), // name: String , department: String
-    Exit,
-    Invalid,
-}
+mod company {
+    use std::collections::HashMap;    
 
-#[derive(Debug)]
-struct Command {
-    op: Operation,
-}
-
-impl Command {
-    fn print_employees(department_list: &mut HashMap<String, Vec<String>>) {
-        for (department, employee_list) in department_list {
-            println!("[{department}]");
-            for employee_name in employee_list {
-                println!("- {employee_name}");
-            }
-            println!("");
-        }
+    #[derive(Debug)]
+    pub enum Operation {
+        GetAll, 
+        GetDepartment(String), // department :String:
+        Add(String, String), // name: String , department: String
+        Exit,
+        Invalid,
     }
 
-    fn print_department(department_list: &mut HashMap<String, Vec<String>>, department: String) {
-        match department_list.get(&department) {
-            Some(employee_list) => {
+    #[derive(Debug)]
+    pub struct Command {
+        pub op: Operation,
+    }
+
+    impl Command {
+        fn print_employees(department_list: &mut HashMap<String, Vec<String>>) {
+            for (department, employee_list) in department_list {
                 println!("[{department}]");
                 for employee_name in employee_list {
                     println!("- {employee_name}");
                 }
-                println!("");                
-            },
-            None => {
-                println!("{department} department is not exist");
+                println!("");
             }
         }
-    }
 
-    fn add_employee(department_list: &mut HashMap<String, Vec<String>>, name: String, department: String) {
-        let department = department_list.entry(department).or_insert(vec![]);
-        department.push(name);
-    }    
-
-    fn execute(self, department_list: &mut HashMap<String, Vec<String>>) {
-        match self.op {
-            Operation::GetAll => Self::print_employees(department_list),
-            Operation::GetDepartment(department) => Self::print_department(department_list, department),
-            Operation::Add(name, department) => Self::add_employee(department_list, name, department),
-            Operation::Invalid => {
-                println!("The input is invalid.")
-            },
-            _ => (),
-        }
-    }
-
-    fn get_command(command: Vec<String>) -> Self {
-        let first_word = command.get(0);
-    
-        match first_word {
-            Some(word) => {
-                match &word[..] {
-                    "Get" => Self::get_command_width_get(command),
-                    "Add" => Self::get_command_width_add(command),
-                    "exit" => Self { op: Operation::Exit },
-                    _ => Self { op: Operation::Invalid },
+        fn print_department(department_list: &mut HashMap<String, Vec<String>>, department: String) {
+            match department_list.get(&department) {
+                Some(employee_list) => {
+                    println!("[{department}]");
+                    for employee_name in employee_list {
+                        println!("- {employee_name}");
+                    }
+                    println!("");                
+                },
+                None => {
+                    println!("{department} department is not exist");
                 }
             }
-            None => Self {op: Operation::Invalid},
         }
+
+        fn add_employee(department_list: &mut HashMap<String, Vec<String>>, name: String, department: String) {
+            let department = department_list.entry(department).or_insert(vec![]);
+            department.push(name);
+        }    
+
+        pub fn execute(self, department_list: &mut HashMap<String, Vec<String>>) {
+            match self.op {
+                Operation::GetAll => Self::print_employees(department_list),
+                Operation::GetDepartment(department) => Self::print_department(department_list, department),
+                Operation::Add(name, department) => Self::add_employee(department_list, name, department),
+                Operation::Invalid => {
+                    println!("The input is invalid.")
+                },
+                _ => (),
+            }
+        }
+
+        pub fn from(command: Vec<String>) -> Self {
+            let first_word = command.get(0);
+        
+            match first_word {
+                Some(word) => {
+                    match &word[..] {
+                        "Get" => Self::get_command(command),
+                        "Add" => Self::add_command(command),
+                        "exit" => Self { op: Operation::Exit },
+                        _ => Self { op: Operation::Invalid },
+                    }
+                }
+                None => Self {op: Operation::Invalid},
+            }
+        }
+
+        // valid format1: vec!["Get",]
+        // valid format2: vec!["Get", "Engineering"]
+        fn is_valid_get_command(command: &Vec<String>) -> bool {
+            // Here, we check only the number of words. 
+            // The actual check would be a more complex condition.
+            match command.len()  {
+                1 | 2 => true,
+                _ => false,
+            }
+        }
+
+        fn get_command(command: Vec<String>) -> Self {
+            if !Self::is_valid_get_command(&command) {
+                return Self { op: Operation::Invalid }
+            }
+
+            // command example: vec!["Get", "Engineering"]        
+            let department = command.get(1);
+            match department {
+                Some(department) => Self {
+                    op: Operation::GetDepartment(department.to_string())
+                },
+                None => Self { op: Operation::GetAll },
+            }
+        }
+
+        // valid format: vec!["Add", "Amir", "to", "Sales"]
+        fn is_valid_add_command(command: &Vec<String>) -> bool {
+            // Here, we check only the number of words. 
+            // The actual check would be a more complex condition.
+            match command.len()  {
+                4 => true,
+                _ => false,
+            }
+        }    
+        fn add_command(command: Vec<String>) -> Self {
+            if !Self::is_valid_add_command(&command) {
+                return Self { op: Operation::Invalid }
+            }
+
+            // command example: vec!["Add", "Amir", "to", "Sales"]   
+            let name = command.get(1).unwrap();
+            let department = command.get(3).unwrap();
+            Self {
+                op: Operation::Add(
+                    name.to_string(),
+                    department.to_string(),
+                )
+            }
+        }
+
     }
-
-    // valid format1: vec!["Get",]
-    // valid format2: vec!["Get", "Engineering"]
-    fn is_valid_get_command(command: &Vec<String>) -> bool {
-        // Here, we check only the number of words. 
-        // The actual check would be a more complex condition.
-        match command.len()  {
-            1 | 2 => true,
-            _ => false,
-        }
-    }
-
-    fn get_command_width_get(command: Vec<String>) -> Self {
-        if !Self::is_valid_get_command(&command) {
-            return Self { op: Operation::Invalid }
-        }
-
-        // command example: vec!["Get", "Engineering"]        
-        let department = command.get(1);
-        match department {
-            Some(department) => Self {
-                op: Operation::GetDepartment(department.to_string())
-            },
-            None => Self { op: Operation::GetAll },
-        }
-    }
-
-    // valid format: vec!["Add", "Amir", "to", "Sales"]
-    fn is_valid_add_command(command: &Vec<String>) -> bool {
-        // Here, we check only the number of words. 
-        // The actual check would be a more complex condition.
-        match command.len()  {
-            4 => true,
-            _ => false,
-        }
-    }    
-    fn get_command_width_add(command: Vec<String>) -> Self {
-        if !Self::is_valid_add_command(&command) {
-            return Self { op: Operation::Invalid }
-        }
-
-        // command example: vec!["Add", "Amir", "to", "Sales"]   
-        let name = command.get(1).unwrap();
-        let department = command.get(3).unwrap();
-        Self {
-            op: Operation::Add(
-                name.to_string(),
-                department.to_string(),
-            )
-        }
-    }
-
 }
-
 
 // return string vector from stdin input
 fn get_string_vector_from_input() -> Vec<String> {
@@ -149,9 +152,9 @@ fn main() {
 
     loop {
         let input = get_string_vector_from_input();
-        let command = Command::get_command(input);
+        let command = company::Command::from(input);
 
-        if let Operation::Exit = command.op { break; }
+        if let company::Operation::Exit = command.op { break; }
         command.execute(&mut departments_list);
     }
 }
